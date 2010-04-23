457a64da120ef5bf65e2e9506c6858c190b3b868
geoff.rich <geoff.rich@2a168e8a-8f38-5d55-c049-9b524898061f>
2010-04-23 00:25:12 +0000

    Clean up goal distance estimate method.

--- Heliopolis/Utilities/PathFinder/PathFinderPoint.cs
+++ Heliopolis/Utilities/PathFinder/PathFinderPoint.cs
@@ -6,14 +6,12 @@ using Microsoft.Xna.Framework;
 
 namespace Heliopolis.Utilities
 {
-   public  class PathFinderPoint : PathFinder<Point>
+   public class PathFinderPoint : PathFinder<Point>
     {
        public override float GoalDistanceEstimate(Point position, Point goalPosition)
        {
-           Point pos = (Point)position;
-           Point goal = (Point)goalPosition;
-           float XDist = pos.X - goal.X;
-           float YDist = pos.Y - goal.Y;
+           float XDist = position.X - goalPosition.X;
+           float YDist = position.Y - goalPosition.Y;
            return (XDist * XDist) + (YDist * YDist);
        }
     }
